966a88dfc2e29f0572d33a18b572435e0cc8f7f0
Wilmer <wilmer.rodriguez-r@mail.escuelaing.edu.co>
2021-02-07 12:28:35 -0500

    Segundo commit

--- VectorComplexLibrary.py
+++ VectorComplexLibrary.py
@@ -1,5 +1,6 @@
-import numpy as np
+# Librería de funciones para vectores y matrices complejos
 def auxiliar(matriz_vector):
+    # función que corrobora si es una matriz o un arreglo devolviendo la cantidad de columnas de este
     try:
         column = len(matriz_vector[0])
         return column
@@ -9,6 +10,7 @@ def auxiliar(matriz_vector):
 
 
 def auxiliar_1(matriz_vector):
+    # función que corrobora si es una matriz o un arreglo devolviendo un valor booleano
     try:
         column = len(matriz_vector[0])
         return True
@@ -18,9 +20,10 @@ def auxiliar_1(matriz_vector):
 
 
 def auxiliar_2(num_complex):
+    # función que conjuga los números complejos
     real = num_complex.real
     imaginaria = -1 * num_complex.imag
-    if imaginaria < 0:
+    if imaginaria <= 0:
         num = str(real) + str(imaginaria) + 'j'
     else:
         num = str(real) + '+' + str(imaginaria) + 'j'
@@ -30,28 +33,38 @@ def auxiliar_2(num_complex):
 def vectorSumaComplex(vector_1,vector_2):
     for i in range(len(vector_1)):
         vector_1[i] = vector_1[i] + vector_2[i]
-    return vector_1
+    return vector_1[:]
 
 
 def vectorInverComplex(vector):
     for i in range(len(vector)):
         vector[i] = -1 * vector[i]
-    return vector
+    return vector[:]
 
 
 def vectorMultEsComplex(vector, escalar):
     for i in range(len(vector)):
         vector[i] = escalar * vector[i]
-    return vector
+    return vector[:]
+
+
+def matrizSumaComplex(vector_1, vector_2):
+    rows = len(vector_1)
+    colums = len(vector_1[0])
+    for i in range(rows):
+        for j in range(colums):
+            vector_1[i][j] = vector_1[i][j] + vector_2[i][j]
+    return vector_1[:]
+
 
 def matrizInverComplex(matriz):
     matriz = [[-1 * matriz[j][k] for k in range(len(matriz[0]))] for j in range(len(matriz))]
-    return matriz
+    return matriz[:]
 
 
 def matrizMultEsComplex(matriz, escalar):
     matriz = [[escalar * matriz[j][k] for k in range(len(matriz[0]))] for j in range(len(matriz))]
-    return matriz
+    return matriz[:]
 
 
 def trasMatrizVector (matriz_vector):
@@ -65,7 +78,7 @@ def trasMatrizVector (matriz_vector):
     return aux
 
 
-def conjMatrizVector (matriz_vector):
+def conjMatrizVector(matriz_vector):
     rows = len(matriz_vector)
     column = auxiliar(matriz_vector)
     if auxiliar_1(matriz_vector):
@@ -78,8 +91,9 @@ def conjMatrizVector (matriz_vector):
     return matriz_vector[:]
 
 
+
 def adjuntMatrizVector (matriz_vector):
-    return trasMatrizVector(conjMatrizVector(matriz_vector))
+    return trasMatrizVector(conjMatrizVector(matriz_vector[:]))
 
 
 def multMatrices (matriz_a, matriz_b):
@@ -95,21 +109,32 @@ def multMatrices (matriz_a, matriz_b):
         return 'Las matrices no se pueden operar'
 
 
-def dotProduct(vector_a,vector_b):
+def dotProduct (vector_a, vector_b):
     size = len(vector_a)
-    suma = 0
-    for j in range(size):
-        suma += vector_a[j] * vector_b[j]
-    return suma
+    try:
+        for i in range(size):
+            int(vector_a[i])
+        suma = 0
+        for j in range(size):
+            suma += vector_a[j] * vector_b[j]
+        return suma
+    except TypeError:
+        vector_a = conjMatrizVector(vector_a[:])
+        suma = 0
+        for j in range(size):
+            suma += complex(vector_a[j]) * complex(vector_b[j])
+        return suma
 
 
 def normVector(vector):
-    return (dotProduct(vector, vector))**(1/2)
+    try:
+        for i in range(len(vector)):
+            int(vector[i])
+        return (dotProduct(vector[:], vector[:]))**(1/2)
+    except TypeError:
+        return (dotProduct(vector[:], vector[:]))**(1/2)
 
 
 def disVectors(vector_a, vector_b):
-    a = np.array(vector_a)
-    b = np.array(vector_b)
-    vector = a - b
+    vector = vectorSumaComplex(vectorInverComplex(vector_a), vector_b)
     return normVector(vector)
-
---
+++ VectorComplexTest.py
@@ -0,0 +1,137 @@
+import unittest
+import math
+import numpy as np
+from random import randint
+import VectorComplexLibrary as vcl
+
+SIZE = 2
+TESTS = 10
+SEED = 1e2
+SEED_2 = 10
+
+def conv(dupla):
+    return complex(str(dupla[0]) + ('+' if dupla[1] >= 0 else '') + (str(dupla[1])) + 'j')
+
+class testfunciones(unittest.TestCase):
+
+    def test_suma_vector(self):
+        for i in range(TESTS):
+            rows = randint(2, SEED_2)
+            vector_a = [conv((randint(-int(SEED), int(SEED)), randint(-int(SEED), int(SEED)))) for j in range(rows)]
+            vector_b = [conv((randint(-int(SEED), int(SEED)), randint(-int(SEED), int(SEED)))) for j in range(rows)]
+            vector_a_np = np.array(vector_a[:])
+            vector_b_np = np.array(vector_b[:])
+            self.assertTrue((vcl.vectorSumaComplex(vector_a[:], vector_b[:]) == vector_a_np + vector_b_np).all())
+
+    def test_inver_vector(self):
+        for i in range(TESTS):
+            rows = randint(2, SEED_2)
+            vector_a = [conv((randint(-int(SEED), int(SEED)), randint(-int(SEED), int(SEED)))) for j in range(rows)]
+            vector_a_np = np.array(vector_a[:])
+            self.assertTrue((vcl.vectorInverComplex(vector_a[:]) == -1 * vector_a_np).all())
+
+    def test_escalar_vector(self):
+        for i in range(TESTS):
+            rows = randint(2, SEED_2)
+            escalar = conv((randint(-int(SEED), int(SEED)), randint(-int(SEED), int(SEED))))
+            vector_a = [conv((randint(-int(SEED), int(SEED)), randint(-int(SEED), int(SEED)))) for j in range(rows)]
+            vector_a_np = np.array(vector_a[:])
+            self.assertTrue((vcl.vectorMultEsComplex(vector_a[:], escalar) == escalar * vector_a_np).all())
+
+    def test_suma_matriz(self):
+        for i in range(TESTS):
+            rows = randint(2, SEED_2)
+            columns = randint(2, SEED_2)
+            matriz_a = [[conv((randint(-int(SEED), int(SEED)), randint(-int(SEED), int(SEED)))) for j in range(rows)]
+                        for k in range(columns)]
+            matriz_b = [[conv((randint(-int(SEED), int(SEED)), randint(-int(SEED), int(SEED)))) for j in range(rows)]
+                        for k in range(columns)]
+            matriz_a_np = np.array(matriz_a[:])
+            matriz_b_np = np.array(matriz_b[:])
+            self.assertTrue((vcl.matrizSumaComplex(matriz_a[:], matriz_b[:]) == matriz_a_np + matriz_b_np).all())
+
+    def test_inver_matriz(self):
+        for i in range(TESTS):
+            rows = randint(2, SEED_2)
+            columns = randint(2, SEED_2)
+            matriz_a = [[conv((randint(-int(SEED), int(SEED)), randint(-int(SEED), int(SEED)))) for j in range(rows)]
+                        for k in range(columns)]
+            matriz_a_np = np.array(matriz_a[:])
+            self.assertTrue((vcl.matrizInverComplex(matriz_a[:]) == -1 * matriz_a_np).all())
+
+    def test_escalar_matriz(self):
+        for i in range(TESTS):
+            rows = randint(2, SEED_2)
+            columns = randint(2, SEED_2)
+            escalar = conv((randint(-int(SEED), int(SEED)), randint(-int(SEED), int(SEED))))
+            matriz_a = [[conv((randint(-int(SEED), int(SEED)), randint(-int(SEED), int(SEED)))) for j in range(rows)]
+                        for k in range(columns)]
+            matriz_a_np = np.array(matriz_a[:])
+            self.assertTrue((vcl.matrizMultEsComplex(matriz_a[:], escalar) == escalar * matriz_a_np).all())
+
+    def test_trasponer_matriz(self):
+        for i in range(TESTS):
+            rows = randint(2, SEED_2)
+            columns = randint(2, SEED_2)
+            matriz_a = [[conv((randint(-int(SEED), int(SEED)), randint(-int(SEED), int(SEED)))) for j in range(rows)]
+                        for k in range(columns)]
+            matriz_a_np = np.array(matriz_a[:])
+            self.assertTrue((vcl.trasMatrizVector(matriz_a[:]) == np.transpose(matriz_a_np)).all())
+
+    def test_conjugar_matriz(self):
+        for i in range(TESTS):
+            rows = randint(2, SEED_2)
+            columns = randint(2, SEED_2)
+            matriz_a = [[conv((randint(-int(SEED), int(SEED)), randint(-int(SEED), int(SEED)))) for j in range(rows)]
+                        for k in range(columns)]
+            matriz_a_np = np.array(matriz_a[:])
+            self.assertTrue((vcl.conjMatrizVector(matriz_a[:]) == np.conj(matriz_a_np)).all())
+
+    def test_adjunta_matriz(self):
+        for i in range(TESTS):
+            rows = randint(2, SEED_2)
+            columns = randint(2, SEED_2)
+            matriz_a = [[conv((randint(-int(SEED), int(SEED)), randint(-int(SEED), int(SEED)))) for j in range(rows)]
+                        for k in range(columns)]
+            matriz_a_np = np.array(matriz_a[:])
+            self.assertTrue((vcl.adjuntMatrizVector(matriz_a[:]) == np.transpose(np.conj(matriz_a_np))).all())
+
+    def test_multi_matriz(self):
+        for i in range(TESTS):
+            rows = randint(2, SEED_2)
+            columns = randint(2, SEED_2)
+            matriz_a = [[conv((randint(-int(SEED), int(SEED)), randint(-int(SEED), int(SEED)))) for j in range(columns)]
+                        for k in range(rows)]
+            matriz_b = [[conv((randint(-int(SEED), int(SEED)), randint(-int(SEED), int(SEED)))) for j in range(rows)]
+                        for k in range(columns)]
+            matriz_a_np = np.array(matriz_a[:])
+            matriz_b_np = np.array(matriz_b[:])
+            self.assertTrue((vcl.multMatrices(matriz_a[:], matriz_b[:]) == np.dot(matriz_a_np, matriz_b_np)).all())
+
+    def test_producto_interno(self):
+        for i in range(TESTS):
+            rows = randint(2, SEED_2)
+            vector_a = [conv((randint(-int(SEED), int(SEED)), randint(-int(SEED), int(SEED)))) for j in range(rows)]
+            vector_b = [conv((randint(-int(SEED), int(SEED)), randint(-int(SEED), int(SEED)))) for j in range(rows)]
+            vector_a_np = np.array(vector_a[:])
+            vector_b_np = np.array(vector_b[:])
+            self.assertTrue((vcl.dotProduct(vector_a[:], vector_b[:]) == np.inner(np.conj(vector_a_np), vector_b_np)).
+                            all())
+
+    def test_norma_vector(self):
+        for i in range(TESTS):
+            rows = randint(2, SEED_2)
+            vector_a = [conv((randint(-int(SEED), int(SEED)), randint(-int(SEED), int(SEED)))) for j in range(rows)]
+            vector_a_np = np.array(vector_a[:])
+            self.assertTrue((vcl.normVector(vector_a[:]) == (np.inner(np.conj(vector_a_np), vector_a_np))**(1/2)).all())
+
+    def test_distancia(self):
+        for i in range(TESTS):
+            rows = randint(2, SEED_2)
+            vector_a = [conv((randint(-int(SEED), int(SEED)), randint(-int(SEED), int(SEED)))) for j in range(rows)]
+            vector_b = [conv((randint(-int(SEED), int(SEED)), randint(-int(SEED), int(SEED)))) for j in range(rows)]
+            vector_c = np.array(vector_a[:]) - np.array(vector_b[:])
+            self.assertTrue((vcl.disVectors(vector_a[:], vector_b[:]) == (np.inner(np.conj(vector_c), vector_c))**(1/2))
+                            .all())
+if __name__ == '__main__':
+    unittest.main()
